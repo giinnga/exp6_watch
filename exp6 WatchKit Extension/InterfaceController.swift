7020ba803addb84ce63e190517ac4cd1cb566040
Victor <victoraugustolls@gmail.com>
2015-06-11 00:03:20 -0700

    Added Menu do select Dolar/Cents, values buttons working also

--- exp6 WatchKit Extension/InterfaceController.swift
+++ exp6 WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,17 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    
+    var actualDolarValue = 0
+    var actualCentsValue = 0
+    
+    @IBOutlet weak var dolarLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var centsLabel: WKInterfaceLabel!
+    
+    var dolarSelected: Bool = true
+    
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -27,5 +38,144 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func changeDolar() {
+        
+        self.dolarSelected = true
+        
+    }
+    
+    @IBAction func changeCents() {
+        
+        self.dolarSelected = false
+        
+    }
 
+    @IBAction func oneMore() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue++
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+        
+        else {
+            
+            self.actualCentsValue++
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    @IBAction func fiveMore() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue += 5
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+            
+        else {
+            
+            self.actualCentsValue += 5
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    @IBAction func tenMore() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue += 10
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+            
+        else {
+            
+            self.actualCentsValue += 10
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    @IBAction func oneLess() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue -= 1
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+            
+        else {
+            
+            self.actualCentsValue -= 1
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    @IBAction func fiveLess() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue -= 5
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+            
+        else {
+            
+            self.actualCentsValue -= 5
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    @IBAction func tenLess() {
+        
+        if self.dolarSelected {
+            
+            self.actualDolarValue -= 10
+            
+            self.dolarLabel.setText("\(self.actualDolarValue)")
+            
+        }
+            
+        else {
+            
+            self.actualCentsValue -= 10
+            
+            self.centsLabel.setText("\(self.actualCentsValue)")
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
